72adbd5f0d6b9623ccee365faa009a5641c4558e
NippoS <86075966+NippoS@users.noreply.github.com>
2021-08-25 21:41:39 +0300

    Add files via upload

---
+++ src/main/java/CRUD/model/Skill.java
@@ -0,0 +1,38 @@
+package CRUD.model;
+
+public class Skill {
+    private Long id;
+    private String name;
+
+    public Skill(Long id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Skill() {
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,23 @@
+import CRUD.model.Developer;
+import CRUD.model.Skill;
+import CRUD.model.Team;
+import CRUD.repository.DeveloperRepository;
+import CRUD.repository.SkillRepository;
+import CRUD.repository.TeamRepository;
+
 public class main {
     public static void main(String[] args) {
+        testAll();
+    }
+
+    public static void testAll(){
         skillTest();
+        devTest();
+        teamTest();
     }
 
     public static void skillTest() {
+        System.out.println("SKILL:");
         SkillRepository sR = new SkillRepository();
         System.out.println(sR.getById(1L));
         System.out.println("Полный список: " + sR.getAll());
@@ -14,22 +28,28 @@ public class main {
         sR.deleteById(3L);
         System.out.println("Удаление: " + sR.getAll());
     }
+    public static void devTest() {
+        System.out.println("DEV:");
+        DeveloperRepository dR = new DeveloperRepository();
+        System.out.println(dR.getById(1L));
+        System.out.println("Полный список: " + dR.getAll());
+        System.out.println("New Dev: " + dR.save(new Developer(null, "python", "c++", null)));
+        System.out.println("Добавление: " + dR.getAll());
+        System.out.println("Update Dev: " + dR.update(new Developer(3L, "python3","c++3", null )));
+        System.out.println("Изменение: " + dR.getAll());
+        dR.deleteById(3L);
+        System.out.println("Удаление: " + dR.getAll());
+    }
+    public static void teamTest() {
+        System.out.println("TEAM:");
+        TeamRepository tR = new TeamRepository();
+        System.out.println(tR.getById(1L));
+        System.out.println("Полный список: " + tR.getAll());
+        System.out.println("New Team: " + tR.save(new Team(null, "python", null)));
+        System.out.println("Добавление: " + tR.getAll());
+        System.out.println("Update Team: " + tR.update(new Team(3L, "python3", null)));
+        System.out.println("Изменение: " + tR.getAll());
+        tR.deleteById(3L);
+        System.out.println("Удаление: " + tR.getAll());
+    }
 }
-
-
-/*
-1. getById ++++
-2. getAll ++++
-3. save ++++
-4. update
-5. delete ++++
- */
-
-/*
-1. getById ++++
-2. getAll ++++
-3. save ++++
-4. update ++++
-5. delete ++++
-6. Ошибки у class Team
- */
